48c3330d725a0944e758ed8ef531d873f5b8bab8
James <jmb47580@ucmo.edu>
2019-08-18 09:31:05 -0500

    Creating the layout

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {Button, StyleSheet, View} from 'react-native';
+import ActionSheet from 'react-native-actionsheet';
+
+export default class App extends Component {
+  showActionSheet = () => {
+    this.ActionSheet.show();
+  };
+
+  render() {
+    //Options to show in bottom action sheet.
+    const optionArray = [
+      'Option 1',
+      'Option 2',
+      'Option 3',
+      'Option 4',
+      'Cancel',
+    ];
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={this.showActionSheet}
+          title="Open Buttom ActionSheet"
+        />
+        <ActionSheet
+          ref={o => (this.ActionSheet = o)}
+          //Title of the Bottom Sheet
+          title={'Which one do you like most ?'}
+          //Options Array to show in bottom sheet
+          options={optionArray}
+          //Define cancel button index in the option array
+          //this will take the cancel option in bottom and will highlight it
+          cancelButtonIndex={4}
+          //If you want to highlight any specific option you can use below prop
+          destructiveButtonIndex={1}
+          onPress={index => {
+            //Clicking on the option will give you the index of the option clicked
+            alert(optionArray[index]);
+          }}
+        />
+      </View>
+    );
+  }
+}
